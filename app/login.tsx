e935d0f9f1f37e14cdf3f76ed8a27d1da5751fec
Al3xand3r11 <alexander.nance11@gmail.com>
2024-08-31 01:16:16 -0700

    changes

--- app/login.tsx
+++ app/login.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import { FaRegStar, FaStar } from "react-icons/fa";
 
 export default function Login() {
     const [userData, setUserData] = useState<any>([]); 
+    const [isStarFilled, setIsStarFilled] = useState<boolean>(false);
     const newData = {
         name: 'test',
         email: 'test@new.com',
@@ -16,7 +18,9 @@ export default function Login() {
         });
     }
 
-    
+    const setStarState = () => {
+        setIsStarFilled(!isStarFilled);
+    }
 
     const checkLogin = async (e: any) => {
         e.preventDefault();
@@ -27,6 +31,12 @@ export default function Login() {
 
     return (
         <div className="h-screen bg-white content-center flex justify-center pt-60 ">
+        <div className="flex flex-col w-full items-center justify-center">
+            <div className="pb-4">
+                <button className="text-black border-4 mx-auto py-2 px-2">
+                    Save Changes
+                </button>
+            </div>
             <div className="border w-6/12 rounded-xl bg-slate-300 h-2/5 ">
                 <div className="pl-4 flex flex-col">
                 <h1 className="text-2xl pt-4">Test users</h1>
@@ -40,6 +50,51 @@ export default function Login() {
                     <input className="border rounded-lg" type="email" name="email" />
     </form> */}
                 </div>
+            </div>
+            <div className="pt-24 grid grid-cols-3 w-full items-center justify-center content-center">
+                <div className="border bg-black w-1/3 h-48 text-center">
+                    <div className="items-center mx-auto p-2">
+                    {isStarFilled ? <FaStar onClick={setStarState} /> : <FaRegStar onClick={setStarState} />
+                    }
+                    Channel One
+                    </div>
+                </div>
+                <div className="border bg-red-700 w-1/3 h-48 text-center">
+                <div className="items-center mx-auto p-2">
+                    {isStarFilled ? <FaStar onClick={setStarState} /> : <FaRegStar onClick={setStarState} />
+                    }
+                    Channel Two
+                    </div>
+                </div>
+                <div className="border bg-emerald-800 w-1/3 h-48 text-center">
+                <div className="items-center mx-auto p-2">
+                    {isStarFilled ? <FaStar onClick={setStarState} /> : <FaRegStar onClick={setStarState} />
+                    }
+                    Channel Three
+                    </div>
+                </div>
+                <div className="border bg-yellow-600 w-1/3 h-48 text-center">
+                <div className="items-center mx-auto p-2">
+                    {isStarFilled ? <FaStar onClick={setStarState} /> : <FaRegStar onClick={setStarState} />
+                    }
+                    Channel Four
+                    </div>
+                </div>
+                <div className="border bg-green-700 w-1/3 h-48 text-center">
+                <div className="items-center mx-auto p-2">
+                    {isStarFilled ? <FaStar onClick={setStarState} /> : <FaRegStar onClick={setStarState} />
+                    }
+                    Channel Five
+                    </div>
+                </div>
+                <div className="border bg-cyan-700 w-1/3 h-48 text-center">
+                <div className="items-center mx-auto p-2">
+                    {isStarFilled ? <FaStar onClick={setStarState} /> : <FaRegStar onClick={setStarState} />
+                    }
+                    Channel Six
+                    </div>
+                </div>
+            </div>
             </div>
         </div>
     );
