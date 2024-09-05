c9c4e9dbd03a29a32c887b3e6c162a3dea504dd4
Al3xand3r11 <alexander.nance11@gmail.com>
2024-09-04 19:08:48 -0700

    sign in

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,5 +1,7 @@
+import { SigninForm } from "@/app/components/SignInForm";
+
 export default function SignInRoute() {
     return <div>
-        <h1>Sign In</h1>
+        <SigninForm/>
     </div>
 }
